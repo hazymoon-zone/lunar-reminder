35191e91177baec3353565297e04f0c7885b0c7c
huyb-cl <huy.bui@codelink.io>
2026-02-10 16:09:18 +0700

    feat: :sparkles: Implement API /reminders using Hono

---
+++ src/lib/lunar.ts
@@ -0,0 +1,53 @@
+import { Lunar, type Solar } from 'lunar-typescript'
+
+export function solarToDate(solar: Solar): Date {
+  const year = solar.getYear()
+  const month = solar.getMonth() - 1
+  const day = solar.getDay()
+
+  return new Date(year, month, day)
+}
+
+export function lunarToDate(lunar: Lunar): Date {
+  const year = lunar.getYear()
+  const month = Math.abs(lunar.getMonth()) + 1
+  const day = lunar.getDay()
+
+  return new Date(year, month, day)
+}
+
+export function dateToLunar(date: Date): Lunar {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+
+  return Lunar.fromYmd(year, month, day)
+}
+
+export function getLunarNextYear(lunar: Lunar): Lunar | null {
+  const currentYear = lunar.getYear()
+  const targetYear = currentYear + 1
+  const month = Math.abs(lunar.getMonth()) // Ignore leap status for anniversary
+  let day = lunar.getDay()
+  // Try to create the date. If it fails (e.g. 30th day in a 29-day month),
+  // then we decide there is no equivalent date in the next year.
+  try {
+    return Lunar.fromYmd(targetYear, month, day)
+  } catch (e) {
+    return null
+  }
+}
+
+export function getLunarNextMonth(lunar: Lunar): Lunar | null {
+  const currentYear = lunar.getYear()
+  const currentMonth = lunar.getMonth()
+  const targetMonth = currentMonth + 1
+  let day = lunar.getDay()
+  // Try to create the date. If it fails (e.g. 30th day in a 29-day month),
+  // then we decide there is no equivalent date in the next month.
+  try {
+    return Lunar.fromYmd(currentYear, targetMonth, day)
+  } catch (e) {
+    return null
+  }
+}
